92e1c14ecd4ca9a3a18e967582c743e8cfdc7502
Jason Ross <jasonrossware@Jasons-MacBook-Pro.local>
2022-12-28 19:17:03 -0800

    big things are happening

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useCallback, useEffect, useState } from 'react';
-import { Button, Dropdown, Input } from 'semantic-ui-react'
+import { Button, Dimmer, Dropdown, Input, Loader } from 'semantic-ui-react'
 import apiRequest from './helpers/api';
 import _ from 'lodash';
-import renderArticlesAndQuotes from './renderers/quotes';
+import renderArticles from './renderers/quotes';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -13,6 +13,8 @@ function App() {
 
   const [searchResult, setSearchResult] = useState(null);
 
+
+  // get list of persons
   useEffect(() => {
     const getPersons = async () => {
       const persons = await apiRequest({ endpoint: 'persons' });
@@ -23,28 +25,68 @@ function App() {
     getPersons();
   }, []);
 
+  // onChanges & onClicks
   const onPersonSelectorChange = (event, result) => {
     const { value } = result || event.target;
     setSelectedPerson(_.find(persons, person => person.id === value));
   }
 
   const search = async () => {
+    setLoading(true);
     const searchResult = await apiRequest({ endpoint: 'articlesByPersonAndPhrase', parameters: { person_id: selectedPerson.id, phrase: keywords, all_forms: 'true' } });
     searchResult.searched_person = selectedPerson;
 
     setSearchResult(searchResult);
+    setLoading(false);
   }
 
+  // renderers
   const renderSearchResults = useCallback(() => {
-    return renderArticlesAndQuotes(searchResult);
+    return renderArticles(searchResult);
   }, [searchResult]);
 
-  if (loading) {
-    return <div>Loading...</div>
-  }
   return (
     <div className="App">
-      <div style={{ minWidth: "600px", display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
+      <Dimmer blurred inverted active={loading}>
+        <Loader />
+      </Dimmer>
+      <div className="header">
+        <div className='header-section'>
+          <h2 className='logo-text'>WhatTheySaid</h2>
+        </div>
+        <div className='header-section'>
+          <div style={{ margin: '0 auto' }}>
+            <Input placeholder="Keywords" onChange={(event) => {setKeywords(event.target.value)}} style={{ marginRight: '3px' }} />
+            <Dropdown
+              placeholder="Select person"
+              search
+              selection
+              options={persons.map(person => {
+                const personFullName = `${person.first_name} ${person.last_name}`;
+                return {
+                  key: person.id,
+                  text: personFullName,
+                  value: person.id,
+                }
+              })}
+              onChange={onPersonSelectorChange}
+              style={{ marginRight: '3px' }}
+            />
+            <Button
+              onClick={search}
+              disabled={!(selectedPerson && keywords)}
+              >
+              Search
+            </Button>
+
+          </div>
+        </div>
+        <div className='header-section'>
+          12 people
+          24,000 articles
+        </div>
+      </div>
+      <div style={{ display: 'none' }}>
         <Input placeholder="Keywords" onChange={(event) => {setKeywords(event.target.value)}} />
         <Dropdown
           placeholder="Select person"
--- src/renderers/quotes.js
+++ src/renderers/quotes.js
@@ -1,35 +1,135 @@
-import { Button } from "semantic-ui-react";
+import _ from 'lodash';
+import { DateTime } from 'luxon';
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { Popup } from 'semantic-ui-react';
 
-export default function renderArticlesAndQuotes(searchResult) {
-  console.log(searchResult);
+function getSentencesFromJsonContent(jsonContent, searchResult) {
+  var jsonContentGroupedBySpeaker = [];
 
+  jsonContent.forEach(articlePiece => {
+    const lastAddedJsonContentGroup = jsonContentGroupedBySpeaker[jsonContentGroupedBySpeaker.length - 1];
+    if (lastAddedJsonContentGroup?.speaker === articlePiece?.speaker) {
+      lastAddedJsonContentGroup.pieces.push(articlePiece.content);
+    } else {
+      jsonContentGroupedBySpeaker.push({ speaker: articlePiece.speaker, pieces: [articlePiece.content] });
+    }
+  })
+
+  return _.flatMap(jsonContentGroupedBySpeaker.map(jsonContentGroup => {
+    if (jsonContentGroup?.speaker === searchResult.searched_person.id) {
+      return _.flatMap(jsonContentGroup.pieces.map(piece => {
+        const separatedSentencesInThisPiece = piece.split(/(?<=[?>!.]\s)/).map(content => {
+          return { type: 'sentence', content };
+        });
+        return separatedSentencesInThisPiece;
+      }))
+    } else {
+      return { type: 'separator', content: `---other-speaker---` }
+    }
+  }));
+}
+
+function getSentencesFromContent(content) {
+  return content.split(/(?<=[?>!.]\s)/);
+}
+
+function renderQuotes(article, searchResult) {
+  const regExpString = `\\b${searchResult.terms_searched.join('\\b|\\b')}\\b`;
+  const regExp = new RegExp(regExpString, "gi");
+
+  const sentences = article?.json_content ? getSentencesFromJsonContent(article.json_content, searchResult) : getSentencesFromContent(article.content);
+
+
+  const matchingSentencesIndexes = sentences.map((sentence, dex) => {
+    if (sentence.type === 'sentence' && regExp.test(sentence.content)) {
+      return dex;
+    } else { return undefined }
+  }).filter(index => index !== undefined);
+
+  var captureGroups = [];
+
+  matchingSentencesIndexes.forEach(sentenceIndex => {
+    const lastCapturedGroup = captureGroups[captureGroups.length - 1];
+    const lastCapturedIndex = lastCapturedGroup ? lastCapturedGroup[lastCapturedGroup.length - 1] : 0;
+    if (captureGroups.length === 0 || (sentenceIndex - lastCapturedIndex) > 2) {
+      captureGroups.push([sentenceIndex]);
+    } else {
+      captureGroups[captureGroups.length - 1].push(sentenceIndex);
+    }
+  })
+
+  const finalCaptureGroups = captureGroups.map(captureGroup => {
+    const beginIndex = captureGroup[0] - 1 < 0 ? 0 : captureGroup[0] - 1;
+    const endIndex = captureGroup[captureGroup.length - 1] + 1 > sentences.length - 1 ? sentences.length - 1 : captureGroup[captureGroup.length - 1] + 1;
+
+    return { start: beginIndex, end: endIndex };
+  })
+
+  const finalQuotes = finalCaptureGroups.map(captureGroup => {
+    return sentences.slice(captureGroup.start, captureGroup.end + 1).map(sentence => sentence.content).join('');
+  })
+
+  function renderQuote(quote, searchResults) {
+    const regExpString = `${searchResult.terms_searched.join('|')}`;
+    const regExp = new RegExp(regExpString, "gi");
+    return quote.split(/\b/).map((word, dex) => {
+      if ((regExp).test(word)) {
+        return <b key={dex}><u>{word}</u></b>
+      }
+      return word;
+    });
+  }
+
+  return finalQuotes.map((quote, dex) => {
+    return (
+      <div key={dex} className='quote-container' style={{  }}>
+        <div>
+          <div className='quote-rating-div' id='upvote-quote-div' style={{  }}>
+            <Popup size='mini' content='Interesting' mouseEnterDelay={500} position='top center' trigger={
+              <IoIosArrowUp/>
+            } />
+          </div>
+          <div className='quote-rating-div'>122</div>
+          <div className='quote-rating-div' id='downvote-quote-div' style={{  }}>
+            <Popup size='mini' content='Not interesting' mouseEnterDelay={500} position='bottom center' trigger={
+              <IoIosArrowDown/>
+            } />
+          </div>
+        </div>
+        <p>
+          {renderQuote(quote, searchResult, regExp)}
+        </p>
+      </div>
+    );
+  })
+
+}
+
+export default function renderArticles(searchResult) {
   if (!searchResult) {
     return 'search something first';
   } else if (searchResult?.articles?.length === 0) {
     return 'no results';
   } else {
-    return searchResult.articles.map(article => {
-      const regExpString = `\\b${searchResult.terms_searched.join('\\b|\\b')}\\b/gi`
-      const regExp = new RegExp(regExpString, "i");
+    const articles = searchResult?.articles;
 
-      const separatedSentenceElements = article.content.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/);
+    return articles.map((article, dex) => {
+      const articleAddedDate = DateTime.fromISO(article.timestamp_added).toLocaleString(DateTime.DATETIME_MED);
 
-      console.log(separatedSentenceElements);
-
-    })
-    return (
-      <div className="result" style={{ backgroundColor: "#f0f0f0", height: "100px" }}>
-        <h3>{`${searchResult?.searched_person?.first_name} ${searchResult?.searched_person?.last_name} said:`}</h3>
-        <p>This is content</p>
-        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-          <Button negative style={{ margin: 0 }}>Less context</Button>
-          <Button positive style={{ margin: 0 }}>More context</Button>
+      return (
+        <div key={dex} className='article-container'>
+          <div className='article-header'>
+            <h4>{article.title}</h4>
+            <p>{articleAddedDate}</p>
+          </div>
+          <div className='article-content'>
+            {renderQuotes(article, searchResult)}
+          </div>
         </div>
-      </div>
-    )
+      );
+    })
   }
-}
 
-function renderQuotesForArticle() {
+
 
 }
